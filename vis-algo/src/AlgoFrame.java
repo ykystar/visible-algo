d4666ffb4a1d447a6f12cb28d126791fe2f3c442
Astros <ykypony@gmail.com>
2019-02-24 22:25:25 +0800

    update template

--- vis-algo/src/AlgoFrame.java
+++ vis-algo/src/AlgoFrame.java
@@ -1,53 +1,77 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.RenderingHints;
 
-public class AlgoFrame extends JFrame {
+import javax.swing.*;
+
+public class AlgoFrame extends JFrame{
+
     private int canvasWidth;
     private int canvasHeight;
 
-    public AlgoFrame(String title,int canvasWidth,int canvasHeight){
+    public AlgoFrame(String title, int canvasWidth, int canvasHeight){
+
         super(title);
+
         this.canvasWidth = canvasWidth;
         this.canvasHeight = canvasHeight;
 
         AlgoCanvas canvas = new AlgoCanvas();
         setContentPane(canvas);
-        pack();
+
+        setResizable(false);
+        pack();    // 在setResizable(false)后进行pack()，防止在Windows下系统修改frame的尺寸
+        // 具体参见：http://coding.imooc.com/learn/questiondetail/26420.html
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
         setVisible(true);
     }
 
     public AlgoFrame(String title){
-        this(title,1024,768);
+
+        this(title, 1024, 768);
     }
 
     public int getCanvasWidth(){return canvasWidth;}
     public int getCanvasHeight(){return canvasHeight;}
 
-    private Circle[] circles;
-    public void render(Circle[] circles){
-        this.circles=circles;
+    // TODO: 设置自己的数据
+    private Object data;
+    public void render(Object data){
+        this.data = data;
         repaint();
     }
 
     private class AlgoCanvas extends JPanel{
+
         public AlgoCanvas(){
+            // 双缓存
             super(true);
         }
 
         @Override
-        public void paintComponent(Graphics g){
+        public void paintComponent(Graphics g) {
             super.paintComponent(g);
+
             Graphics2D g2d = (Graphics2D)g;
 
-            RenderingHints hints = new RenderingHints(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-            hints.put(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+            // 抗锯齿
+            RenderingHints hints = new RenderingHints(
+                    RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
             g2d.addRenderingHints(hints);
 
+            // 具体绘制
+            // TODO： 绘制自己的数据data
+        }
 
-
+        @Override
+        public Dimension getPreferredSize(){
+            return new Dimension(canvasWidth, canvasHeight);
         }
     }
 }
+
