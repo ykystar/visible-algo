1283a0040953c5d17dc1f17ad2c710243a24e9e2
Astros <ykypony@gmail.com>
2019-02-25 22:29:16 +0800

    update

---
+++ Maze-Solver/Maze-Files-Processing/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        String mazeFile = "101.txt";
+        MazeData data = new MazeData(mazeFile);
+        data.print();
+    }
+}
