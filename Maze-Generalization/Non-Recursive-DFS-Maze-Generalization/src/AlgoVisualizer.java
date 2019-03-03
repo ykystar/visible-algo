44ca9b106807673921a5fe898dff099c12d59986
Astros <ykystar@163.com>
2019-03-03 09:42:34 +0800

    updata

--- Maze-Generalization/Non-Recursive-DFS-Maze-Generalization/src/AlgoVisualizer.java
+++ Maze-Generalization/Non-Recursive-DFS-Maze-Generalization/src/AlgoVisualizer.java
@@ -1,48 +1,74 @@
+
+import java.util.Stack;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class AlgoVisualizer {
 
-    // TODO: 创建自己的数据
-    private Object data;        // 数据
-    private AlgoFrame frame;    // 视图
+    private static int DELAY = 20;
+    private static int blockSide = 8;
 
-    public AlgoVisualizer(int sceneWidth, int sceneHeight){
+    private MazeData data;
+    private AlgoFrame frame;
+    private static final int d[][] = {{-1,0},{0,1},{1,0},{0,-1}};
+
+    public AlgoVisualizer(int N, int M){
 
         // 初始化数据
-        // TODO: 初始化数据
+        data = new MazeData(N, M);
+        int sceneHeight = data.N() * blockSide;
+        int sceneWidth = data.M() * blockSide;
 
         // 初始化视图
         EventQueue.invokeLater(() -> {
-            frame = new AlgoFrame("Welcome", sceneWidth, sceneHeight);
-            // TODO: 根据情况决定是否加入键盘鼠标事件监听器
-            frame.addKeyListener(new AlgoKeyListener());
-            frame.addMouseListener(new AlgoMouseListener());
+            frame = new AlgoFrame("Random Maze Generation Visualization", sceneWidth, sceneHeight);
+
             new Thread(() -> {
                 run();
             }).start();
         });
     }
 
-    // 动画逻辑
     private void run(){
 
-        // TODO: 编写自己的动画逻辑
+        setData(-1, -1);
+
+        Stack<Position> stack = new Stack<Position>();
+        Position first = new Position(data.getEntranceX(), data.getEntranceY()+1);
+        stack.push(first);
+        data.visited[first.getX()][first.getY()] = true;
+
+        while(!stack.empty()){
+            Position curPos = stack.pop();
+
+            for(int i = 0 ; i < 4  ; i ++){
+                int newX = curPos.getX() + d[i][0]*2;
+                int newY = curPos.getY() + d[i][1]*2;
+
+                if(data.inArea(newX, newY) && !data.visited[newX][newY]){
+                    stack.push(new Position(newX, newY));
+                    data.visited[newX][newY] = true;
+                    setData(curPos.getX() + d[i][0], curPos.getY() + d[i][1]);
+                }
+            }
+        }
+
+        setData(-1, -1);
     }
 
-    // TODO: 根据情况决定是否实现键盘鼠标等交互事件监听器类
-    private class AlgoKeyListener extends KeyAdapter{ }
-    private class AlgoMouseListener extends MouseAdapter{ }
+    private void setData(int x, int y){
+        if(data.inArea(x, y))
+            data.maze[x][y] = MazeData.ROAD;
+
+        frame.render(data);
+        AlgoVisHelper.pause(DELAY);
+    }
 
     public static void main(String[] args) {
 
-        int sceneWidth = 800;
-        int sceneHeight = 800;
+        int N = 101;
+        int M = 101;
 
-        // TODO: 根据需要设置其他参数，初始化visualizer
-        AlgoVisualizer visualizer = new AlgoVisualizer(sceneWidth, sceneHeight);
+        AlgoVisualizer vis = new AlgoVisualizer(N, M);
+
     }
 }
